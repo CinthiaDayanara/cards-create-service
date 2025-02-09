01b9b268539a48666dceb0867db4bb393d735bb1
CinthiaDayanara <cdchuraco@uce.edu.ec>
2025-02-09 13:10:56 -0500

    prueba4

--- src/routes/cardsRoutes.js
+++ src/routes/cardsRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { createCard, getCards } = require("../controllers/cardsController");
 const router = express.Router();
 
-router.post("/cards", createCard);  // Para agregar una tarjeta
-router.get("/cards", getCards);  // Para obtener todas las tarjetas
+router.post("/", createCard);  // Para agregar una tarjeta
+router.get("/", getCards);  // Para obtener todas las tarjetas
 
 module.exports = router;
